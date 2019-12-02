b6ae9eba396ccaa3eb0a2c66f38868f53f285795
Francesco Bruno <b.frenc@hotmail.it>
2019-12-02 20:29:15 +0100

    beginning

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', loadChildren: "./main/main.module.ts#MainModule", canActivate: [AuthGuard] },
+  { path: 'auth', loadChildren: "./auth/auth.module.ts#AuthModule" },
+  { path: 'onboarding', loadChildren: "./onboarding/onboarding.module.ts#OnboardingModule" },
+  { path:'', pathMatch:'full', redirectTo:'/dashboard'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
