419aaba75f4c31e74efd1d83e255c754cbf4b314
Francesco Bruno <b.frenc@hotmail.it>
2019-12-02 21:44:55 +0100

    loading, nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
+import { MainModule } from './main/main.module';
+import { AuthModule } from './auth/auth.module';
+import { OnboardingModule } from './onboarding/onboarding.module';
 
 const routes: Routes = [
-  { path: 'dashboard', loadChildren: "./main/main.module.ts#MainModule", canActivate: [AuthGuard] },
-  { path: 'auth', loadChildren: "./auth/auth.module.ts#AuthModule" },
-  { path: 'onboarding', loadChildren: "./onboarding/onboarding.module.ts#OnboardingModule" },
+  { path: 'dashboard', loadChildren: () => MainModule, canActivate: [AuthGuard] },
+  { path: 'auth', loadChildren: () => AuthModule },
+  { path: 'onboarding', loadChildren: () => OnboardingModule },
   { path:'', pathMatch:'full', redirectTo:'/dashboard'}
 ];
 
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MainComponent } from './main/main.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [DashboardComponent, MainComponent],
+  declarations: [DashboardComponent, MainComponent, NavComponent],
   imports: [
     CommonModule,
     MainRoutingModule
